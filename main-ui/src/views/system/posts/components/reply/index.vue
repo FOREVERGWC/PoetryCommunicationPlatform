4eebfa2556fc6b830a7aa925210fcaaf713b10eb
瑶 <916586595@qq.com>
2024-05-17 11:42:21 +0800

    楼中楼优化

--- main-ui/src/views/system/posts/components/reply/index.vue
+++ main-ui/src/views/system/posts/components/reply/index.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row v-for="(item, index) in comments" :key="item.id">
       <el-col :span="1">
-        <el-avatar :src="getAvatar(item.user.avatar)" style="border: 1px solid gray"></el-avatar>
+        <el-avatar :src="getAvatar(item.user.avatar)" shape="square" style="border: 1px solid gray"></el-avatar>
       </el-col>
       <el-col :span="23">
         <span style="color: #fb7299">{{ item.user.nickName || item.user.userName }}</span>
@@ -18,17 +18,43 @@
               IP属地：{{ item.location }}
             </span>
             <span style="margin-left: 10px">
-              来自<span style="color: #00afff">{{ item.os }}客户端</span>
+              来自<span style="color: #00afff; cursor: pointer">{{ item.os }}客户端</span>
             </span>
             <span style="margin-left: 10px">
               {{ index + 2 }}楼
             </span>
           </span>
         </el-row>
-        <Reply v-if="item.children && item.children.length > 0" :comments="item.children" :get-avatar="getAvatar" :handle-reply-person="handleReplyPerson"/>
+        <el-row v-for="child in item.children" :key="child.id">
+          <el-col :span="1">
+            <el-avatar :src="getAvatar(child.user.avatar)" shape="square" style="border: 1px solid gray"></el-avatar>
+          </el-col>
+          <el-col :span="23">
+            <span style="color: #fb7299">{{ child.user.nickName || child.user.userName }}</span>
+            <el-row v-if="child.replyId !== item.id">
+              <span style="margin-right: 5px">回复</span>
+              <span style="color: #00afff; cursor: pointer">@{{ getItem(item.children, child.replyId) }}</span>
+            </el-row>
+            <el-row>{{ child.content }}</el-row>
+            <el-row>
+              <span style="color: gray">{{ child.createTime }}</span>
+              <svg-icon icon-class="favour" style="margin-left: 20px"/>
+              0
+              <svg-icon icon-class="dis-favour" style="margin-left: 20px"/>
+              <span style="color: gray; cursor: pointer; margin-left: 20px" @click="handleReplyPerson(child)">回复</span>
+              <span style="color: gray; float: right">
+                <span>
+                  IP属地：{{ child.location }}
+                </span>
+                <span style="margin-left: 10px">
+                  来自<span style="color: #00afff; cursor: pointer">{{ child.os }}客户端</span>
+                </span>
+              </span>
+            </el-row>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
-    <el-divider></el-divider>
   </div>
 </template>
 
@@ -39,13 +65,30 @@ export default {
     comments: Array,
     handleReplyPerson: Function
   },
+  data() {
+    return {
+      list: []
+    }
+  },
   computed: {
     getAvatar() {
       return (path) => {
         return path === '' || path === null ? require("@/assets/images/profile.jpg") : process.env.VUE_APP_BASE_API + path
       }
+    },
+    getReplyName() {
+      return (list, id) => {
+        return list.map(item.id === id).userName
+      }
     }
   },
+  methods: {
+    getItem(children, targetId) {
+      const item = children.find(item => item.id === targetId)
+      console.log(item, targetId)
+      return item?.user.nickName || item?.user.userName
+    }
+  }
 }
 </script>
 
--- main-ui/src/views/system/posts/detail.vue
+++ main-ui/src/views/system/posts/detail.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <el-card shadow="hover" style="margin: 20px">{{ form.title }}</el-card>
+    <el-card shadow="hover" style="margin: 20px">{{ info.title }}</el-card>
     <el-card style="margin-left: 20px; margin-right: 20px">
       <el-row>
         <el-col :span="2">
           <el-row style="align-items: center">
-            <el-avatar :src="require('@/assets/images/profile.jpg')" shape="square" style="border: 1px solid gray"></el-avatar>
+            <el-avatar :src="avatar" shape="square" style="border: 1px solid gray"></el-avatar>
           </el-row>
           <el-row style="align-items: center; margin-top: 5px">
-            <span>{{ form.createBy }}</span>
+            <span>{{ info.createBy }}</span>
           </el-row>
         </el-col>
         <el-col :span="22">
-          <span v-html="form.content"></span>
+          <span v-html="info.content"></span>
           <el-row>
-            <span style="color: gray; float: right">1楼&emsp;{{ form.createTime }}</span>
+            <span style="color: gray; float: right">1楼&emsp;{{ info.createTime }}</span>
           </el-row>
         </el-col>
       </el-row>
@@ -24,16 +24,20 @@
         <h1 style="display: inline-block; margin: 0;">回复</h1>
         <div class="total" style="display: inline-block; margin-left: 15px">{{ total }}</div>
       </div>
-      <span style="cursor: pointer" @click="getReplyList('asc')">正序</span>
+      <span :class="sort === 'asc' ? 'sort' : ''" style="cursor: pointer" @click="getReplyList('asc')">正序</span>
       <el-divider direction="vertical"></el-divider>
-      <span style="cursor: pointer" @click="getReplyList('desc')">倒序</span>
+      <span :class="sort === 'desc' ? 'sort' : ''" style="cursor: pointer" @click="getReplyList('desc')">倒序</span>
       <el-row>
         <el-col :span="1">
           <el-avatar :src="avatar" style="border: 1px solid gray"></el-avatar>
         </el-col>
         <el-col :span="23">
           <span v-if="replyId" style="padding-top: 5px; padding-bottom: 5px">回复<span style="color: #66ccff">@{{ replyName }}</span></span>
-          <el-input v-model="reply" placeholder="一时的嘴臭，极致的享受" type="textarea"></el-input>
+          <el-form ref="info" :model="form" :rules="rules">
+            <el-form-item prop="reply">
+              <el-input v-model="form.reply" placeholder="一时的嘴臭，极致的享受" type="textarea"></el-input>
+            </el-form-item>
+          </el-form>
         </el-col>
       </el-row>
       <el-row>
@@ -57,17 +61,21 @@ export default {
   data() {
     return {
       avatar: store.getters.avatar,
-      form: {},
+      info: {},
       replyList: [],
       total: 0,
-      reply: '',
       replyId: null,
-      replyName: ''
+      replyName: '',
+      sort: 'asc',
+      form: {},
+      rules: {
+        reply: [{required: true, message: '回复内容不能为空', trigger: 'blur'}]
+      }
     }
   },
   created() {
     const postsId = this.$route.params && this.$route.params.postsId
-    this.form.id = postsId
+    this.info.id = postsId
     this.getDetail(postsId)
     this.getReplyList('asc')
   },
@@ -81,37 +89,41 @@ export default {
   methods: {
     getDetail(postsId) {
       readPosts(postsId).then(res => {
-        this.form = res.data
+        this.info = res.data
       })
     },
     getReplyList(type) {
       const form = {
         orderByColumn: 'create_time',
         isAsc: type === 'asc' ? 'asc' : 'desc',
-        postsId: this.form.id
+        postsId: this.info.id
       }
+      this.sort = type
       listReply(form).then(res => {
         this.replyList = res.rows
         this.total = res.title
       })
     },
     handleReply() {
-      const form = {
-        content: this.reply,
-        postsId: this.form.id,
-        replyId: this.replyId
-      }
-      addReply(form).then(res => {
-        if (res.code === 200) {
-          this.reply = ''
-          this.replyId = null
-          this.replyName = ''
-          this.$message.success('回复成功！')
-        } else {
-          this.$message.error('回复失败！')
+      this.$refs.info.validate(valid => {
+        if (!valid) return
+        const form = {
+          content: this.form.reply,
+          postsId: this.info.id,
+          replyId: this.replyId
         }
-      }).finally(() => {
-        this.getReplyList('des')
+        addReply(form).then(res => {
+          if (res.code === 200) {
+            this.form.reply = ''
+            this.replyId = null
+            this.replyName = ''
+            this.$message.success('回复成功！')
+          } else {
+            this.$message.error('回复失败！')
+          }
+        }).finally(() => {
+          this.getReplyList('desc')
+        })
       })
     },
     handleReplyPerson(item) {
@@ -131,5 +143,10 @@ export default {
 .el-row {
   padding-top: 10px;
   padding-bottom: 10px;
+}
+
+::v-deep .sort {
+  cursor: pointer;
+  font-weight: bold;
 }
 </style>
